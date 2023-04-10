6614da112fbd798c95ebf73bd53cba54d6daa44d
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-10 22:50:18 +0530

    accidentally deleted loop

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "jenkins_folder" "folders" {
 }
 
 resource "jenkins_job" "job" {
-    depends_on = [
-      jenkins_folder.folders
-    ]
+    depends_on = [jenkins_folder.folders]
+    
+  count    = length(var.jobs)
   name     = lookup(element(var.jobs, count.index), "name", null)
   folder   = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"  #if /job is not mentioned then it everytime getting deleted
   template = templatefile("${path.module}/singleBranch-pipeline.xml", {
