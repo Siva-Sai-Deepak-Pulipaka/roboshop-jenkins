2f2f5fb9f4b34893efbfdb11385947e3f0c8382c
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-10 20:55:02 +0530

    protocol

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "jenkins" {
-  server_url = "https://172.31.10.122:8080" # Or use JENKINS_URL env var
+  server_url = "http://172.31.10.122:8080" # Or use JENKINS_URL env var
   username   = data.aws_ssm_parameter.jenkins_user.value            # Or use JENKINS_USERNAME env var
   password   = data.aws_ssm_parameter.jenkins_pass.value            # Or use JENKINS_PASSWORD env var
 }
